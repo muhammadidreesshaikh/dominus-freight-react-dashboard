86e726e1c1c17ee5d5f9ff6f552a8b92b8111f92
Muhammad Awais <awaisshaikh.official@gmail.com>
2020-10-08 14:47:44 +0500

    dashboard init

---
+++ src/views/Dashboard.jsx
@@ -0,0 +1,81 @@
+
+import React, { Component } from "react";
+import ChartistGraph from "react-chartist";
+import { Grid, Row, Col } from "react-bootstrap";
+
+import { Card } from "components/Card/Card.jsx";
+import { StatsCard } from "components/StatsCard/StatsCard.jsx";
+import { Tasks } from "components/Tasks/Tasks.jsx";
+import {
+  dataPie,
+  legendPie,
+  dataSales,
+  optionsSales,
+  responsiveSales,
+  legendSales,
+  dataBar,
+  optionsBar,
+  responsiveBar,
+  legendBar
+} from "variables/Variables.jsx";
+
+class Dashboard extends Component {
+  createLegend(json) {
+    var legend = [];
+    for (var i = 0; i < json["names"].length; i++) {
+      var type = "fa fa-circle text-" + json["types"][i];
+      legend.push(<i className={type} key={i} />);
+      legend.push(" ");
+      legend.push(json["names"][i]);
+    }
+    return legend;
+  }
+  render() {
+    return (
+      <div className="content">
+        <Grid fluid>
+          <Row>
+            <Col lg={3} sm={6}>
+              <StatsCard
+                bigIcon={<i className="pe-7s-server text-warning" />}
+                statsText="Capacity"
+                statsValue="105GB"
+                statsIcon={<i className="fa fa-refresh" />}
+                statsIconText="Updated now"
+              />
+            </Col>
+            <Col lg={3} sm={6}>
+              <StatsCard
+                bigIcon={<i className="pe-7s-wallet text-success" />}
+                statsText="Revenue"
+                statsValue="$1,345"
+                statsIcon={<i className="fa fa-calendar-o" />}
+                statsIconText="Last day"
+              />
+            </Col>
+            <Col lg={3} sm={6}>
+              <StatsCard
+                bigIcon={<i className="pe-7s-graph1 text-danger" />}
+                statsText="Errors"
+                statsValue="23"
+                statsIcon={<i className="fa fa-clock-o" />}
+                statsIconText="In the last hour"
+              />
+            </Col>
+            <Col lg={3} sm={6}>
+              <StatsCard
+                bigIcon={<i className="fa fa-twitter text-info" />}
+                statsText="Followers"
+                statsValue="+45"
+                statsIcon={<i className="fa fa-refresh" />}
+                statsIconText="Updated now"
+              />
+            </Col>
+          </Row>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
