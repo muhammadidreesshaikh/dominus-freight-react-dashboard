62f6b2b07467e09cd1fb628c041eff369a1c0fe1
Muhammad Idrees <idreesiqbal104@gmail.com>
2020-10-10 18:44:06 +0500

    Updates

---
+++ precache-manifest.c1a615807d96be597b3488d492c916a3.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1783501756af5065d59cf880623d39b",
+    "url": "/dominus-freight-react-dashboard/index.html"
+  },
+  {
+    "revision": "1a91e80c34a12b3fd069",
+    "url": "/dominus-freight-react-dashboard/static/css/2.b7f966a4.chunk.css"
+  },
+  {
+    "revision": "44b8e050b2c9f3c18bb3",
+    "url": "/dominus-freight-react-dashboard/static/css/main.379f6e92.chunk.css"
+  },
+  {
+    "revision": "1a91e80c34a12b3fd069",
+    "url": "/dominus-freight-react-dashboard/static/js/2.0eec19e4.chunk.js"
+  },
+  {
+    "revision": "44b8e050b2c9f3c18bb3",
+    "url": "/dominus-freight-react-dashboard/static/js/main.cf8f417c.chunk.js"
+  },
+  {
+    "revision": "5dab812bcd3d78cc92d1",
+    "url": "/dominus-freight-react-dashboard/static/js/runtime~main.7c456273.js"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "/dominus-freight-react-dashboard/static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "/dominus-freight-react-dashboard/static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/dominus-freight-react-dashboard/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "c45f7de008ab976a8e817e3c0e5095ca",
+    "url": "/dominus-freight-react-dashboard/static/media/Pe-icon-7-stroke.c45f7de0.svg"
+  },
+  {
+    "revision": "ce7a6b79aa55041f7ae36f6ce22231fe",
+    "url": "/dominus-freight-react-dashboard/static/media/face-3.ce7a6b79.jpg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/dominus-freight-react-dashboard/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/dominus-freight-react-dashboard/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/dominus-freight-react-dashboard/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/dominus-freight-react-dashboard/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/dominus-freight-react-dashboard/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "9b864b3680f372ff9ae02649fbc44838",
+    "url": "/dominus-freight-react-dashboard/static/media/reactlogo.9b864b36.png"
+  },
+  {
+    "revision": "6be21e8a1b7d63048728851c6003e189",
+    "url": "/dominus-freight-react-dashboard/static/media/sidebar-1.6be21e8a.jpg"
+  },
+  {
+    "revision": "7a4ce7cc040fc1cb8176cde106e9232f",
+    "url": "/dominus-freight-react-dashboard/static/media/sidebar-2.7a4ce7cc.jpg"
+  },
+  {
+    "revision": "34535b958ad0f0183770a8731b31b4ca",
+    "url": "/dominus-freight-react-dashboard/static/media/sidebar-3.34535b95.jpg"
+  },
+  {
+    "revision": "7bc259fd5cef15e95376c822c7e23081",
+    "url": "/dominus-freight-react-dashboard/static/media/sidebar-4.7bc259fd.jpg"
+  }
+]);
